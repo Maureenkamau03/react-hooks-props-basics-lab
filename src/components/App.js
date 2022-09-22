29786635627a972a8a2b1084ebd54081b76124fd
Maureenkamau03 <mkaymaureen@gmail.com>
2022-09-22 20:40:15 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,13 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home name={user.name} city={user.city} color={user.color} />
+			<About bio={user.bio} links={user.links} />
+		</div>
+	);
 }
 
 export default App;
